3512321087f5cf359d3ca291e728dd636bfe8b40
Peter Truong <petergtruong@gmail.com>
2024-04-02 23:38:45 -0700

    Update Icon.gd

--- Icon.gd
+++ Icon.gd
@@ -8,9 +8,18 @@ var x = coins + SPEED
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	minion()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rotate_y(deg_to_rad(SPEED))
+	check_inputs()
+
+func minion():
+	print("Hey!")
+
+func check_inputs():
+	if Input.is_action_pressed("ui_left"):
+		rotate_y(deg_to_rad(-SPEED))
+	elif Input.is_action_pressed("ui_right"):
+		rotate_y(deg_to_rad(SPEED))
